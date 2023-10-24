6ac3b9981bfdd98aa85ffccabed45d7d9d4f6906
Ayush Tripathi <yush.pbh@gmail.com>
2023-10-25 01:20:33 +0530

    Liked Songs toggle feature implemented

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,7 @@
-var player = document.getElementById('myaudiopalyer');
+var player = document.getElementById('myaudiopalyer'); 
 var albumart = document.getElementById('albumart'); 
+
+var likedSongs = Array();
 
 var currentPlayingSong = 0;
 
@@ -8,11 +10,14 @@ var picList = ["pic1.jpg", "pic2.jpg", "pic3.jpg"]
 
 
 
+function likeCurrentPlayingSong(){
+    likeASong(currentPlayingSong);
+}
 
 function nextSong(){
     if(currentPlayingSong >= songList.length - 1){
         currentPlayingSong = 0;
-     }
+    }
     else{
         currentPlayingSong = currentPlayingSong + 1;
     }
@@ -40,17 +45,15 @@ function playSong(){
     player.src = "./assets/" + songList[currentPlayingSong] +".mp3";
     // Play the player  
     player.play();
-    
     // Updating the current Song Playing Status
-    var playerStaus = document.getElementById('playerStatus');
-    playerStaus.innerText = "Current Playing : "+songList[currentPlayingSong];
-
+    var playerStatus = document.getElementById('playerStatus');
+    playerStatus.innerText = "Current Playing : "+songList[currentPlayingSong];
     //Change the albumart of the current song
     albumart.style.backgroundImage = "url('./assets/" +picList[currentPlayingSong]+ "')";
     // Console log Done!
     console.log("Done!");
-
 }
+
 var isPlaying = false;
 function togglePlayPause(){
     if(isPlaying){
@@ -91,7 +94,6 @@ function searchSong(){
     var mySongList = document.getElementsByClassName('songList')[0];
     // search for song in song list
     // Performing Linear Search here!
-    var resultsArray = Array();
     
     mySongList.innerHTML = "";
 
@@ -100,7 +102,7 @@ function searchSong(){
         if (songList[i].includes(query)) {
             // resultsArray.push(songList[i]);
       
-    var _s = '<div class="song" onclick="playSongFromUI('+i+')" ><div class="songAlbumArt" style="background-image: url(\'./assets/pic'+(i+1)+'.jpg\') "></div><h1 class="songName">My Song</h1></div>';
+        var _s = '<div class="song"><div class="songAlbumArt" style="background-image: url(\'./assets/pic'+(i+1)+'.jpg\') " onclick="playSongFromUI('+i+')"></div><h1 class="songName">'+songList[i]+'</h1><div class="likedSongIcon" onclick="likeASong('+i+')"></div></div>';
 
             mySongList.innerHTML += _s;
         }
@@ -108,4 +110,21 @@ function searchSong(){
     // now create new song List for UI
     // change Ui
     // Done!
+}
+
+function likeASong(index){
+    // check whether song is liked or not
+    if(likedSongs.includes(index)){
+        var myLikedSongIndex = likedSongs.indexOf(index);
+        likedSongs.splice(myLikedSongIndex,1);
+    }
+    else{
+        likedSongs.push(index);
+    }
+}
+
+function printLikedSongs(){
+    for(var i = 0; i<likedSongs.length; i++){
+        console.log(likedSongs[i]);
+    }
 }
